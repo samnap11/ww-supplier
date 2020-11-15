3164962abce92f8e0b1760187a9e02fa74301af9
donbasta <faddilafarras@gmail.com>
2020-11-15 09:59:42 +0700

    make querying db and sending response async

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get("/url", (req, res, next) => {
 
 
 // Middleware for handling transaction logic
-processTransaction = function (req, res, next) {
+processTransaction = async function (req, res, next) {
     console.log("Transaction request: ");
     console.log(req.body);
     console.log(req.body.buyList);
@@ -39,42 +39,58 @@ processTransaction = function (req, res, next) {
     let buyList = req.body.buyList;
     console.log("buyList", buyList);
     let payAmount = req.body.payAmount;
-    let totalCost = 0;
-    
 
+    function getCostForSupply(query, amount) {
+        return new Promise ((resolve, reject) => {
+            db_con.query(query, function(err, result) {
+                if(err) {
+                    reject ("database error");
+                    throw err;
+                }
+                console.log(result);
+                cost = result[0].harga_satuan;
+                console.log("cost: ", cost);
+                // totalCost += cost * supplyAmount;
+                resolve(cost * amount);
+            })
+        })
+    }
+
+    let costList = [];
     for (let buy of buyList) {
         console.log("buy", buy);
         supplyName = buy.nama;
         supplyAmount = buy.amount;
         console.log("supply: ", supplyName, supplyAmount);
         const query = `SELECT * FROM bahan WHERE nama_bahan = '${supplyName}'`;
-        db_con.query(query, function(err, result) {
-            if(err) throw err;
-            console.log(result);
-            cost = result[0].harga_satuan;
-            console.log("cost: ", cost);
-            totalCost += cost * supplyAmount;
+        const cost = await getCostForSupply(query, supplyAmount);
+        costList.push(cost);
+    }
+
+    Promise.all(costList)
+        .then(() => {
+            const totalCost = costList.reduce((a, b) => a + b)
+            console.log("totalCost: ", totalCost);
+            let data = {};
+            if (payAmount < totalCost) {
+                // - memberikan respon jumlah uang yang kurang dan status transaksi
+                const res = totalCost - payAmount;
+                const status = "failed"; // status gagal
+                data = {"status": status, "need": res,};
+            } else {
+                // - memberikan respon jumlah uang hasil transaksi dan status transaksi (??what is jumlah uang hasil transaksi??)
+                const res = totalCost;
+                const status = "success"; // status berhasil
+                data = {"status": status, "value": res,};
+            }
+        
+            res.locals.data = data;
+        
+            return next();
         })
-    }
-
-    console.log("totalCost: ", totalCost);
-
-    let data = {};
-    if (payAmount < totalCost) {
-        // - memberikan respon jumlah uang yang kurang dan status transaksi
-        const res = totalCost - payAmount;
-        const status = "failed"; // status gagal
-        data = {"status": status, "need": res,};
-    } else {
-        // - memberikan respon jumlah uang hasil transaksi dan status transaksi (??what is jumlah uang hasil transaksi??)
-        const res = totalCost;
-        const status = "success"; // status berhasil
-        data = {"status": status, "value": res,};
-    }
-
-    res.locals.data = data;
-
-    return next();
+        .catch((err) => {
+            console.log(err);
+        })
 }
 
 // Melakukan transaksi bahan dengan Factory Management Pro
