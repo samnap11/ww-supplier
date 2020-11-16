b0f22e4bfbe95026ecf28ef4917addd9e812a67f
hengkysuryaa <hengkysurya88@gmail.com>
2020-11-16 21:11:37 +0700

    query parameter utk tampilin harga/tdk di json

--- routes/supply.js
+++ routes/supply.js
@@ -3,7 +3,18 @@ const supplyRouter = express.Router();
 const db_con = require('../config/dbConnection');
 
 supplyRouter.get('/', (req, res) => {
-  const query = 'SELECT * FROM bahan';
+  
+  harga = req.query.harga;
+  let query = '';
+  if (harga === '0') {
+    query = 'SELECT id_bahan, nama_bahan FROM bahan';  
+  } else if (harga === '1') {
+    query = 'SELECT * FROM bahan'; 
+  } else {
+    console.log('invalid value of query parameter')
+  }
+  
+  //const query = 'SELECT id_bahan, nama_bahan FROM bahan';
   db_con.query(query, function (err, result) {
     if (err) throw err;
     let dat = [];
